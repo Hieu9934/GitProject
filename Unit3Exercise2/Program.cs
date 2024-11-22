1b111b5536ec2bd5f4b8975f81fc937e73797a92
Hieu9934 <43hieu1999@gmail.com>
2024-11-22 22:00:09 +0700

    finish exercise 2 of unit 3

---
+++ Unit3Exercise2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Please provide your price in USD: $");
+
+string input = Console.ReadLine();
+
+int vndPrice;
+bool isNumber = int.TryParse(input, out vndPrice);
+
+if (isNumber){
+    vndPrice *= 23000;
+    Console.WriteLine("It is equivalent to " + vndPrice + " VND.");
+} else {
+    Console.WriteLine("You provide an invalid input.");
+}
