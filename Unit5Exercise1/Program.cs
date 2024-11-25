71d3a6e7c79a307a2f0e083b7fb320a3dfc7c63a
Hieu9934 <43hieu1999@gmail.com>
2024-11-25 20:34:21 +0700

    Finish Unit 5 Exercise 1

---
+++ Unit5Exercise1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Numerics;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Draw a rectangle");
+            Console.WriteLine("2. Draw a square triangle");
+            Console.WriteLine("3. Draw an isosceles triangle");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter your choice: ");
+            Int32.TryParse(Console.ReadLine(), out int choice);
+
+            // Keep asking until choosing Exit
+            while (choice != 4)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        for (int i = 0; i < 5; i++)
+                        {
+                            for (int j = 0; j < 15; j++)
+                            {
+                                Console.Write("*");
+                            }
+                            Console.WriteLine();
+                        }
+                        break;
+                    case 2:
+                        for (int i = 0; i < 5; i++)
+                        {
+                            for (int j = 0; j <= i; j++)
+                            {
+                                Console.Write("*");
+                            }
+                            Console.WriteLine();
+                        }
+                        break;
+                    case 3:
+                        for (int i = 0; i < 5; i++)
+                        {
+                            for (int j = 0; j < 4 - i; j++)
+                            {
+                                Console.Write(" ");
+                            }
+                            for (int k = 0; k < i * 2 + 1; k++)
+                            {
+                                Console.Write("*");
+                            }
+                            Console.WriteLine();
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("The choice is invalid!");
+                        break;
+                }
+
+                Console.Write("You may choose again: ");
+                Int32.TryParse(Console.ReadLine(), out choice);
+            }
+
+            Environment.Exit(0);
+        }
+    }
+}
